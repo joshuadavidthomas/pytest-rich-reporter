745ff8dcb4fc3207567ed7879ba58543694157e7
Josh <josh@joshthomas.dev>
2022-02-16 15:38:36 -0600

    add basic richreporter

--- src/pytest_rich/plugin.py
+++ src/pytest_rich/plugin.py
@@ -2,7 +2,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import pytest
+
+from .console import console
+from .reporter import RichReporter
+
 if TYPE_CHECKING:
+    from _pytest.config import Config
     from _pytest.config.argparsing import Parser
 
 
@@ -20,3 +26,20 @@ def pytest_addoption(parser: Parser):
         default=False,
         help="Enable rich output for the terminal reporter (default: False)",
     )
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_configure(config: Config):
+    """
+    Configure the rich plugin
+
+    :param config: The pytest config object
+    """
+    use_rich = getattr(config.option, "rich", False)
+
+    if use_rich:
+        standard_reporter = config.pluginmanager.get_plugin("terminalreporter")
+        rich_reporter = RichReporter(config, console)
+
+        config.pluginmanager.unregister(standard_reporter)
+        config.pluginmanager.register(rich_reporter, "terminalreporter")
